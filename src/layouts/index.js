02b3e8251ba19417e58a3a69bfe05f7609d93377
Luis Egan <luisdegan10@hotmail.com>
2018-02-22 20:03:25 -0300

    heroku deploy

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,8 +8,21 @@ import Helmet from "react-helmet";
 import Header from "../components/Header";
 import "./index.scss";
 
+const jquery = (
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" />
+);
+const popper = (
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" />
+);
+const bootstrap = (
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" />
+);
+
 const TemplateWrapper = ({ children }) => (
   <div>
+    {jquery}
+    {popper}
+    {bootstrap}
     <Helmet
       title="Advir.co"
       meta={[
