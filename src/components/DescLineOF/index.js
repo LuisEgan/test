97b388b68f48153acfa2e7655d8721da6a5818a4
Luis Egan <luisdegan10@hotmail.com>
2018-02-28 13:39:59 -0300

    play on mount

--- src/components/DescLineOF/index.js
+++ src/components/DescLineOF/index.js
@@ -5,58 +5,71 @@ import { Player } from "video-react";
 import laptopBase from "../../assets/imgs/laptopBase.png";
 import logo from "../../assets/imgs/logo-white.png";
 
-const DescLineOF = ({
-  title,
-  text,
-  imgSrc,
-  videoSrc,
-  videoType,
-  youtubeId,
-  Component
-}) => {
-  return (
-    <div className="container description-line-overflow-img row">
-      <div className="description  col-sm-12 col-md-3">
-        <h3 className="st">{title}</h3>
-        <br />
-        <span className="mb">{text}</span>
-        {title === "1. Install" && (
-          <Link to="/download" className="nav-link btn white-btn">
-            Download Advir
-          </Link>
-        )}
-      </div>
-      <div className="image col-sm-12 col-md-9 laptop-screen">
-        {/* Priority: 1. local video, 2. youtube video and 3. Image */}
-        {/* If all are provided, the local video is going to be displayed, and so on. */}
-        <div>
-          {!!videoSrc && (
-            <Player playsInline muted={true} src={videoSrc[0]} />
-            // <video loop autoPlay muted playsInline poster={logo}>
-            //   {videoSrc.map((src, i) => {
-            //     return <source src={src} type={`video/${videoType[i]}`} />;
-            //   })}
-            // </video>
+class DescLineOF extends React.Component {
+  componentDidMount() {
+    !!this.refs.player && this.refs.player.play();
+  }
+  render() {
+    const {
+      title,
+      text,
+      imgSrc,
+      videoSrc,
+      videoType,
+      youtubeId,
+      Component
+    } = this.props;
+    return (
+      <div className="container description-line-overflow-img row">
+        <div className="description  col-sm-12 col-md-3">
+          <h3 className="st">{title}</h3>
+          <br />
+          <span className="mb">{text}</span>
+          {title === "1. Install" && (
+            <Link to="/download" className="nav-link btn white-btn">
+              Download Advir
+            </Link>
           )}
-          {!!youtubeId &&
-            !videoSrc && (
-              <iframe
-                width="560"
-                height="315"
-                src={`https://www.youtube.com/embed/${youtubeId}?rel=0&amp;showinfo=0`}
-                frameborder="0"
-                allow="autoplay; encrypted-media"
-                allowfullscreen
+        </div>
+        <div className="image col-sm-12 col-md-9 laptop-screen">
+          {/* Priority: 1. local video, 2. youtube video and 3. Image */}
+          {/* If all are provided, the local video is going to be displayed, and so on. */}
+          <div>
+            {!!videoSrc && (
+              <Player
+                playsInline
+                muted={true}
+                // autoPlay={true}
+                preload="auto"
+                src={videoSrc[0]}
+                ref="player"
               />
+              // <video loop autoPlay muted playsInline poster={logo}>
+              //   {videoSrc.map((src, i) => {
+              //     return <source src={src} type={`video/${videoType[i]}`} />;
+              //   })}
+              // </video>
             )}
-          {!!imgSrc && !videoSrc && !youtubeId && <img src={imgSrc} />}
-          {!!Component && !videoSrc && !youtubeId && !imgSrc && <Component />}
+            {!!youtubeId &&
+              !videoSrc && (
+                <iframe
+                  width="560"
+                  height="315"
+                  src={`https://www.youtube.com/embed/${youtubeId}?rel=0&amp;showinfo=0`}
+                  frameborder="0"
+                  allow="autoplay; encrypted-media"
+                  allowfullscreen
+                />
+              )}
+            {!!imgSrc && !videoSrc && !youtubeId && <img src={imgSrc} />}
+            {!!Component && !videoSrc && !youtubeId && !imgSrc && <Component />}
 
-          {!Component && <img src={laptopBase} className="laptop-base" />}
+            {!Component && <img src={laptopBase} className="laptop-base" />}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default DescLineOF;
