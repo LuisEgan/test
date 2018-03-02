3ba262f24474397556c4c14a17ec4ce9dae568de
Luis Egan <luisdegan10@hotmail.com>
2018-03-02 13:50:00 -0300

    bo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 // import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../../node_modules/bootstrap/dist/js/bootstrap.js";
+// import "../../node_modules/bootstrap/dist/js/bootstrap.js";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
