3b82e6ab5c9460d2cffb1e375842eb9182ab21d4
Luis Egan <luisdegan10@hotmail.com>
2018-03-02 14:20:02 -0300

    style contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,8 @@ import Link from "gatsby-link";
 
 const Contact = () => (
   <div id="contact">
-    <div className="container row first-show">
+    <div className="container row">
+      <h3 className="st">Get in touch</h3>
       <div
         className="pipedriveWebForms"
         data-pd-webforms="https://pipedrivewebforms.com/form/96519734f454d60cf788c842689c212e2720848"
